49ad8614fe4e7aee491e47960c93d4b8c0fbb00b
lwairore <kwairore@gmail.com>
2020-07-09 22:25:04 +0300

    - Generate AboutMeComponent section.

---
+++ src/app/core/about-me/about-me.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mra-about-me',
+  templateUrl: './about-me.component.html',
+  styles: [
+  ]
+})
+export class AboutMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
