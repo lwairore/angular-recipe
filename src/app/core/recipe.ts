e1265223689e2c16820c563196a48f5964049649
lwairore <kwairore@gmail.com>
2020-07-09 22:19:18 +0300

    - Format document

--- src/app/core/recipe.ts
+++ src/app/core/recipe.ts
@@ -1,2 +1,9 @@
 export class Recipe {
+    constructor(
+        public name: string,
+        public title: string,
+        public image: string,
+        public rating: number,
+        public durationInMinutes: number
+    ) { }
 }
