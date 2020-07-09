51bef99f045e557f08210e339c6c2c204cf7dda1
lwairore <kwairore@gmail.com>
2020-07-09 19:38:46 +0300

    - Move the MinutesToTimePipe to shared

--- src/app/minutes-to-time.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MinutesToTimePipe } from './minutes-to-time.pipe';
-
-describe('MinutesToTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MinutesToTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
