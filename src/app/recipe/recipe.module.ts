41a87ad1fdaf89a9df5a8bc024d80328e8bb3546
lwairore <kwairore@gmail.com>
2020-07-09 09:02:29 +0300

    - Register HomepageComponent.

--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomepageComponent,
+  ],
   imports: [
     CommonModule
   ]
