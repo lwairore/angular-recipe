67950b72f01d404c9783775d5b25187e49828b50
lwairore <kwairore@gmail.com>
2020-07-09 22:22:51 +0300

    - Import the CoreModule. - Import the SharedModule.

--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
-
+import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     HomepageComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule,
+    SharedModule,
   ]
 })
 export class RecipeModule { }
