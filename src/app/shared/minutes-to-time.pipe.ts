a1c680f8c1b4353887a54f2029a4fd9dee0c69f6
lwairore <kwairore@gmail.com>
2020-07-09 22:20:42 +0300

    - Format the duration it takes to prepare a recipe in a human readable format.

--- src/app/shared/minutes-to-time.pipe.ts
+++ src/app/shared/minutes-to-time.pipe.ts
@@ -5,8 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MinutesToTimePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number): any {
+    if (!isNaN(value)) {
+      if (value < 60) {
+        return value.toString() + ' min';
+      }
+      else {
+        const hours = Math.floor(value / 60);
+        const minutes = Math.floor(value - (hours * 60));
+
+        if (minutes > 0) {
+          return hours.toString() + ' hr ' + minutes.toString() + ' min';
+        }
+        else {
+          return hours.toString() + ' hr';
+        }
+      }
+
+    }
+    return;
   }
 
 }
