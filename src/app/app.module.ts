4d1f8f785283d6096623386a5e9fc4b02adbc721
lwairore <kwairore@gmail.com>
2020-07-09 09:02:39 +0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeModule } from './recipe/recipe.module';
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
   ],
   imports: [
     BrowserModule,
