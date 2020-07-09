026c6f738c225e2ceee9599b92a270cdecdce741
lwairore <kwairore@gmail.com>
2020-07-09 22:47:37 +0300

    - Separate fullName to firstName and lastName.

--- src/app/core/about-me.ts
+++ src/app/core/about-me.ts
@@ -1,2 +1,9 @@
 export class AboutMe {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public bio: string,
+        public avatar: string
+    ) { }
 }
