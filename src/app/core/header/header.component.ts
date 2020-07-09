9628e7c37cf0210e146c32cf55f1435eb2f00b35
lwairore <kwairore@gmail.com>
2020-07-09 10:20:58 +0300

    - Generate the HeaderComponent.

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mra-header',
+  templateUrl: './header.component.html',
+  styles: [
+  ]
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
