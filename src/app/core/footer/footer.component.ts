6ab4ab48b093e41a1ab6eb5aa136ff0cf3633938
lwairore <kwairore@gmail.com>
2020-07-09 11:21:28 +0300

    - Generate FooterComponent.

---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mra-footer',
+  templateUrl: './footer.component.html',
+  styles: [
+  ]
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
