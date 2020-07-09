ae10ab4b08e7c284153680e4878bbc3f67e2a26d
lwairore <kwairore@gmail.com>
2020-07-09 11:21:44 +0300

    - Import the FooterComponent.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    HeaderComponent,
   ]
 })
 export class CoreModule { }
