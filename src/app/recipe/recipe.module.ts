8a4b482cc205847ac477215aa31b7d6f7389f579
lwairore <kwairore@gmail.com>
2020-07-14 21:37:44 +0300

    - Register SpecificRecipeDetailsComponent.

--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
+import { SpecificRecipeDetailsComponent } from './specific-recipe-details/specific-recipe-details.component';
 
 @NgModule({
   declarations: [
     HomepageComponent,
+    SpecificRecipeDetailsComponent,
   ],
   imports: [
     CommonModule,
