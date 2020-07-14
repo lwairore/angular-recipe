30dc43eb72497633762ca00515f056a371666e9d
lwairore <kwairore@gmail.com>
2020-07-14 21:37:21 +0300

    - Generate SpecificRecipeDetailsComponent.

---
+++ src/app/recipe/specific-recipe-details/specific-recipe-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mra-specific-recipe-details',
+  templateUrl: './specific-recipe-details.component.html',
+  styles: [
+  ]
+})
+export class SpecificRecipeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
