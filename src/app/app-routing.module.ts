334e924b239ff4720e03c1679e8d69c98fd3089a
lwairore <kwairore@gmail.com>
2020-07-14 21:55:03 +0300

    - Add parameterised URL for SpecificRecipeDetailsComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './recipe/homepage/homepage.component';
+import { SpecificRecipeDetailsComponent } from './recipe/specific-recipe-details/specific-recipe-details.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
+  { path: 'recipe/:recipeName', component: SpecificRecipeDetailsComponent },
 ];
 
 @NgModule({
