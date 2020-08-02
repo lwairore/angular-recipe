9187e6d70466091c9f522592622ef021a7135046
lwairore <kwairore@gmail.com>
2020-08-02 14:55:02 +0300

    - Implement OnDestroy lifecycle hook on SpecificRecipeDetailsComponent. - Declare recipeName property that only accepts strings. - Declare recipeDetails property that only accepts an object that has the same properties as Recipe. - Inject ActivatedRoute service as activatedRoute. - Inject RecipeService service as recipeService. - Reassign recipeName to have the value of recipeName in the paramMap in the current URL. - Reassign recipeDetails to have the returned value by getRecipe() method in RecipeService. - Add ngOnDestroy() method.

--- src/app/recipe/specific-recipe-details/specific-recipe-details.component.ts
+++ src/app/recipe/specific-recipe-details/specific-recipe-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { RecipeService } from '../../core/recipe.service';
+import { Recipe } from '../../core/recipe';
 
 @Component({
   selector: 'mra-specific-recipe-details',
@@ -6,11 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SpecificRecipeDetailsComponent implements OnInit {
+export class SpecificRecipeDetailsComponent implements OnInit, OnDestroy {
+  recipeName: string;
+  recipeDetails: Recipe;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipeService: RecipeService
+  ) { 
+    
+  }
 
   ngOnInit(): void {
+    this.recipeName = this.activatedRoute.snapshot.paramMap.get('recipeName')
+    this.recipeDetails = this.recipeService.getRecipe(this.recipeName);
+  }
+
+  ngOnDestroy():void {
   }
 
 }
