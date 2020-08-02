6a08e1212aaf2a3f77f639a2fe35ce8c32a0171e
lwairore <kwairore@gmail.com>
2020-08-02 14:41:52 +0300

    - Add serves property. - Add prepTimeInMinutes property. - Add calories property. - Add description property. - Add ingredients property. - Add directions property. - Add reviews property.

--- src/app/core/recipe.ts
+++ src/app/core/recipe.ts
@@ -4,6 +4,14 @@ export class Recipe {
         public title: string,
         public image: string,
         public rating: number,
-        public durationInMinutes: number
+        public durationInMinutes: number,
+        public sliderImages: Array<string>,
+        public serves: number,
+        public prepTimeInMinutes: number,
+        public calories: number,
+        public description: string,
+        public ingredients: Array<string>,
+        public directions: Array<string>,
+        public reviews?: Array<{}>
     ) { }
 }
