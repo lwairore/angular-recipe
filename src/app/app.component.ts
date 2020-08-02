42adf04d95b28099d2b014e801dcd46271feb7d7
lwairore <kwairore@gmail.com>
2020-08-02 21:12:02 +0300

    - Add offline support turns the web page to grayscle format.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mra-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'recipe';
+export class AppComponent implements OnInit {
+  
+  displayNetworkStatus(): void {
+    if (navigator.onLine){
+      document.querySelector('body').style.filter = ``;
+    } else {
+      document.querySelector('body').style.filter = 'grayscale(1)';
+    }
+  }
+
+  ngOnInit(): void {
+    this.displayNetworkStatus();
+    window.addEventListener('online', this.displayNetworkStatus);
+    window.addEventListener('offline', this.displayNetworkStatus);
+  }
+  
 }
