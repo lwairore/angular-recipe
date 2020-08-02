7ad8ae74df38b43439d15f1f2f7b947d646fe9cc
lwairore <kwairore@gmail.com>
2020-08-02 14:49:33 +0300

    - Declare RecipeBackgroundComponent, and SliderComponent. - Import RouterModule.

--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -4,16 +4,22 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 import { SpecificRecipeDetailsComponent } from './specific-recipe-details/specific-recipe-details.component';
+import { RouterModule } from '@angular/router';
+import { RecipeBackgroundComponent } from './recipe-background/recipe-background.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     HomepageComponent,
     SpecificRecipeDetailsComponent,
+    RecipeBackgroundComponent,
+    SliderComponent,
   ],
   imports: [
     CommonModule,
     CoreModule,
     SharedModule,
+    RouterModule,
   ]
 })
 export class RecipeModule { }
